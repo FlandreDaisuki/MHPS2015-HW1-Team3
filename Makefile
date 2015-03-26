16a4f89f29097dc6a8883ccefdb05ca80db9afae
FlandreDaisuki <vbnm123c@gmail.com>
2015-03-26 11:14:25 +0800

    Gen a set of output

--- Makefile
+++ Makefile
@@ -1,4 +1,20 @@
-all:
-	g++ -std=c++11 *.cpp -o tabu && ./tabu
+all: build twenty fifty	hundred clean
+twenty:
+	./tabu data/tai20_5_1.txt
+	./tabu data/tai20_10_1.txt
+	./tabu data/tai20_20_1.txt
+fifty:
+	./tabu data/tai50_5_1.txt
+	./tabu data/tai50_10_1.txt
+	./tabu data/tai50_20_1.txt
+hundred:
+	./tabu data/tai100_5_1.txt
+	./tabu data/tai100_10_1.txt
+	./tabu data/tai100_20_1.txt
+debug: build
+	./tabu
+	make clean
+build:
+	g++ -std=c++11 *.cpp -o tabu
 clean:
-	rm tabu
+	rm ./tabu
--- main.cpp
+++ main.cpp
@@ -8,17 +8,27 @@ int main(int argc, char const *argv[])
 {
     srand(time(NULL));
     
-    std::string fpath("data/");
-    if(argc > 1) {
-        fpath += argv[1];
+    std::string finpath;
+    std::string foutpath("output");
+
+    if(argc >= 2) {
+        finpath = argv[1];
+        if(argc >= 3) {
+            foutpath = argv[2];
+        } else {
+            foutpath += argv[1];
+        }
     } else {
-        fpath += "debug";
+        finpath = "data/debug.txt";
+        foutpath = "outputdata/debug.txt";
     }
 
     int job, machine;
     std::string fname;
     std::ifstream fin;
-    fin.open(fpath.c_str());
+    std::ofstream fout;
+    fin.open(finpath.c_str());
+    fout.open(foutpath.c_str());
 
     fin >> job >> machine >> fname;
 
@@ -32,13 +42,15 @@ int main(int argc, char const *argv[])
             fin >> schedule.getMatrix()[i][j];
         }
     }
-    schedule.Print();
+    schedule.Print(fout);
     
 
     const int TIMES_TO_HYBRID = 5000;
     const int SOLUTION_TO_GEN = 50;
     const int NEIGHBORHOOD_SIZE = 10;
     const int TABULIST_SIZE = 10;
+
+    clock_t tStart = clock();
     for (int sol = 0; sol < SOLUTION_TO_GEN; ++sol)
     {
         Tabulist tabulist(TABULIST_SIZE);
@@ -52,8 +64,12 @@ int main(int argc, char const *argv[])
         }
         solution.Push(nb.getValue());
     }
+    clock_t tEnd = clock();
 
-    solution.Print();
+    solution.Print(fout);
+
+    fout << std::endl;
+    fout << "Total time: " << (double)(tEnd-tStart)/CLOCKS_PER_SEC <<" sec"<< std::endl;
 
     return 0;
 }
